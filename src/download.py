6fb7ceccca6e7424aa1edeff2f54bf2de7a301fc
Julian Espinel <julianespinel@gmail.com>
2016-03-19 07:24:26 -0500

    Download stock historical data using parameters.

--- src/download.py
+++ src/download.py
@@ -1,11 +1,21 @@
 import urllib
 import requests
+from datetime import date
 
-baseUrl = "https://query.yahooapis.com/v1/public/yql"
-query = urllib.parse.quote('select * from yahoo.finance.historicaldata where symbol = "YHOO" and startDate = "2015-03-11" and endDate = "2016-03-11"')
-resultFormat = 'json'
-env = urllib.parse.quote("store://datatables.org/alltableswithkeys")
-url = baseUrl + "?" + "q=" + query + "&format=" + resultFormat + "&env=" + env
-response = requests.get(url)
-print(response.url)
-print(response.content)
+
+def getStockHistorycalData(quote, initialDate, finalDate):
+    baseUrl = "https://query.yahooapis.com/v1/public/yql"
+    selectStatement = 'select * from yahoo.finance.historicaldata where symbol = "{quote}" and startDate = "{initialDate}" and endDate = "{finalDate}"'.format(quote=quote, initialDate=initialDate, finalDate=finalDate)
+    query = urllib.parse.quote(selectStatement)
+    resultFormat = 'json'
+    env = urllib.parse.quote("store://datatables.org/alltableswithkeys")
+    url = baseUrl + "?" + "q=" + query + "&format=" + resultFormat + "&env=" + env
+    response = requests.get(url)
+    print(response.url)
+    print(response.content)
+
+quote = "YHOO"
+today = date.today()
+initialDate = today.replace(year=today.year-1)
+finalDate = today
+getStockHistorycalData(quote, initialDate, finalDate)
