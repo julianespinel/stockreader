a98ddb1c34da839dd5a83ae663db8c6eea2e7da5
Julian Espinel <julianespinel@gmail.com>
2016-03-21 17:06:53 -0500

    Add saveStockHistoricalData and saveStockCurrentData functions.

--- src/save.py
+++ src/save.py
@@ -1,12 +1,34 @@
+import pymongo
 from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError, BulkWriteError
 
 client = MongoClient()
 db = client["stockboarddb"]
 
 # Do not use map and filter for side effects: http://stackoverflow.com/a/18433519/2420718
 def saveStockList(stocks):
-    stocklistCollection = db["stocklist"]
-    for stock in stocks:
-        stockExists = (stocklistCollection.count(stock) > 0)
-        if not stockExists:
-            stocklistCollection.insert_one(stock)
+    if len(stocks) > 0:
+        try:
+            stocklistCollection = db["stocklist"]
+            stocklistCollection.create_index([("quote", pymongo.ASCENDING)], unique=True)
+            stocklistCollection.insert_many(stocks)
+        except (DuplicateKeyError, BulkWriteError) as err:
+            print(err)
+
+def saveStockHistoricalData(quote, stockHistoricalDataArray):
+    if len(stockHistoricalDataArray) > 0:
+        try:
+            stockHistoricalDataCollection = db[quote + "_historical_data"]
+            stockHistoricalDataCollection.create_index([("Symbol", pymongo.ASCENDING), ("Date", pymongo.DESCENDING)], unique=True)
+            stockHistoricalDataCollection.insert_many(stockHistoricalDataArray)
+        except (DuplicateKeyError, BulkWriteError) as err:
+            print(err)
+
+def saveStockCurrentData(quote, stockCurrentData):
+    if stockCurrentData is not None:
+        try:
+            stockCurrentDataCollection = db["stocks_current_data"]
+            stockCurrentDataCollection.create_index([("symbol", pymongo.ASCENDING)], unique=True)
+            stockCurrentDataCollection.insert_one(stockCurrentData)
+        except DuplicateKeyError as err:
+            print(err)
