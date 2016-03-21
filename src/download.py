9b239c9de82d156ec25b26d952bf8331e2e0a008
Julian Espinel <julianespinel@gmail.com>
2016-03-21 17:04:44 -0500

    Return the expected results from Yahoo finance.

--- src/download.py
+++ src/download.py
@@ -2,20 +2,32 @@ import urllib
 import requests
 from datetime import date
 
-
-def getStockHistorycalData(quote, initialDate, finalDate):
+def getStockHistoricalData(initialDate, finalDate, quote):
     baseUrl = "https://query.yahooapis.com/v1/public/yql"
     selectStatement = 'select * from yahoo.finance.historicaldata where symbol = "{quote}" and startDate = "{initialDate}" and endDate = "{finalDate}"'.format(quote=quote, initialDate=initialDate, finalDate=finalDate)
     query = urllib.parse.quote(selectStatement)
     resultFormat = 'json'
     env = urllib.parse.quote("store://datatables.org/alltableswithkeys")
     url = baseUrl + "?" + "q=" + query + "&format=" + resultFormat + "&env=" + env
-    response = requests.get(url)
-    print(response.url)
-    print(response.content)
+    response = requests.get(url).json()
+    count = response["query"]["count"]
+    if count > 0:
+        stockHistoricalDataArray = response["query"]["results"]["quote"]
+    else:
+        stockHistoricalDataArray = []
+    return stockHistoricalDataArray
 
-quote = "YHOO"
-today = date.today()
-initialDate = today.replace(year=today.year-1)
-finalDate = today
-getStockHistorycalData(quote, initialDate, finalDate)
+def getStockCurrentData(quote):
+    baseUrl = "https://query.yahooapis.com/v1/public/yql"
+    selectStatement = 'select * from yahoo.finance.quote where symbol in ("{quote}")'.format(quote=quote)
+    query = urllib.parse.quote(selectStatement)
+    resultFormat = 'json'
+    env = urllib.parse.quote("store://datatables.org/alltableswithkeys")
+    url = baseUrl + "?" + "q=" + query + "&format=" + resultFormat + "&env=" + env
+    response = requests.get(url).json()
+    count = response["query"]["count"]
+    if count > 0:
+        stockCurrentData = response["query"]["results"]["quote"]
+    else:
+        stockCurrentData = None
+    return stockCurrentData
