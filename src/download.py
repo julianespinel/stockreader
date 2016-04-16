5d96a478fe334a7128f8fbbdaa9e25dcc26a91f3
Julian Espinel <julianespinel@gmail.com>
2016-04-16 08:13:15 -0500

    Make download a class to improve testability.

--- src/download.py
+++ src/download.py
@@ -1,33 +1,34 @@
 import urllib
 import requests
-from datetime import date
 
-def getStockHistoricalData(initialDate, finalDate, quote):
-    baseUrl = "https://query.yahooapis.com/v1/public/yql"
-    selectStatement = 'select * from yahoo.finance.historicaldata where symbol = "{quote}" and startDate = "{initialDate}" and endDate = "{finalDate}"'.format(quote=quote, initialDate=initialDate, finalDate=finalDate)
-    query = urllib.parse.quote(selectStatement)
-    resultFormat = 'json'
-    env = urllib.parse.quote("store://datatables.org/alltableswithkeys")
-    url = baseUrl + "?" + "q=" + query + "&format=" + resultFormat + "&env=" + env
-    response = requests.get(url).json()
-    count = response["query"]["count"]
-    if count > 0:
-        stockHistoricalDataArray = response["query"]["results"]["quote"]
-    else:
-        stockHistoricalDataArray = []
-    return stockHistoricalDataArray
+class Download:
 
-def getStockCurrentData(quote):
-    baseUrl = "https://query.yahooapis.com/v1/public/yql"
-    selectStatement = 'select * from yahoo.finance.quote where symbol in ("{quote}")'.format(quote=quote)
-    query = urllib.parse.quote(selectStatement)
-    resultFormat = 'json'
-    env = urllib.parse.quote("store://datatables.org/alltableswithkeys")
-    url = baseUrl + "?" + "q=" + query + "&format=" + resultFormat + "&env=" + env
-    response = requests.get(url).json()
-    count = response["query"]["count"]
-    if count > 0:
-        stockCurrentData = response["query"]["results"]["quote"]
-    else:
-        stockCurrentData = None
-    return stockCurrentData
+    def getStockHistoricalData(self, initialDate, finalDate, quote):
+        baseUrl = "https://query.yahooapis.com/v1/public/yql"
+        selectStatement = 'select * from yahoo.finance.historicaldata where symbol = "{quote}" and startDate = "{initialDate}" and endDate = "{finalDate}"'.format(quote=quote, initialDate=initialDate, finalDate=finalDate)
+        query = urllib.parse.quote(selectStatement)
+        resultFormat = 'json'
+        env = urllib.parse.quote("store://datatables.org/alltableswithkeys")
+        url = baseUrl + "?" + "q=" + query + "&format=" + resultFormat + "&env=" + env
+        response = requests.get(url).json()
+        count = response["query"]["count"]
+        if count > 0:
+            stockHistoricalDataArray = response["query"]["results"]["quote"]
+        else:
+            stockHistoricalDataArray = []
+        return stockHistoricalDataArray
+
+    def getStockCurrentData(self, quote):
+        baseUrl = "https://query.yahooapis.com/v1/public/yql"
+        selectStatement = 'select * from yahoo.finance.quote where symbol in ("{quote}")'.format(quote=quote)
+        query = urllib.parse.quote(selectStatement)
+        resultFormat = 'json'
+        env = urllib.parse.quote("store://datatables.org/alltableswithkeys")
+        url = baseUrl + "?" + "q=" + query + "&format=" + resultFormat + "&env=" + env
+        response = requests.get(url).json()
+        count = response["query"]["count"]
+        if count > 0:
+            stockCurrentData = response["query"]["results"]["quote"]
+        else:
+            stockCurrentData = None
+        return stockCurrentData
