c4348741241ebddafa03f33e312f3fc682e8c787
Julian Espinel <julianespinel@gmail.com>
2016-04-16 08:15:41 -0500

    Add test to the method downloadAndSaveStockCurrentData from the Domain class.

--- test/testDomain.py
+++ test/testDomain.py
@@ -1,18 +1,40 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import Mock
 from src.domain import Domain
+from src.download import Download
+from src.mongo import Mongo
 
 class DomainTest(unittest.TestCase):
 
     def setUp(self):
-        self.mongoMock = MagicMock()
-        self.downloadMock = MagicMock()
+        self.mongoMock = Mongo()
+        self.downloadMock = Download()
         self.domain = Domain(self.mongoMock, self.downloadMock)
 
     def testDownloadAndSaveStockCurrentData(self):
-        print(self.domain.YEARS_AGO)
-        self.assertEqual(10, self.domain.YEARS_AGO)
+        quote = "BAC"
+        stock = { "name": "Bank of America", "quote": quote, "stockMarket": "NYSE" }
+        stockCurrentData = {
+            "symbol" : "BAC",
+            "DaysLow" : "13.780",
+            "StockExchange" : "NYQ",
+            "DaysRange" : "13.780 - 13.940",
+            "Volume" : "3255146",
+            "LastTradePriceOnly" : "13.925",
+            "YearHigh" : "18.480",
+            "MarketCapitalization" : "146.44B",
+            "Name" : "Bank of America Corporation Com",
+            "AverageDailyVolume" : "136399008",
+            "Change" : "+0.135",
+            "DaysHigh" : "13.940",
+            "Symbol" : "BAC",
+            "YearLow" : "10.990"
+        }
+        self.downloadMock.getStockCurrentData = Mock(return_value=stockCurrentData)
+        self.mongoMock.upsertStockCurrentData = Mock()
+        self.domain.downloadAndSaveStockCurrentData(stock)
+        self.downloadMock.getStockCurrentData.assert_called_once_with(quote)
+        self.mongoMock.upsertStockCurrentData.assert_called_once_with(quote, stockCurrentData)
 
 if __name__ == "main":
-    print("***** hello!")
     unittest.main()
