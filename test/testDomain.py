28711395229237216a76aa6988f9ed46690fadce
Julian Espinel <julianespinel@gmail.com>
2016-04-23 08:53:28 -0500

    Add unit test for the method downloadAndSaveStockHistoricalData from the class domain.

--- test/testDomain.py
+++ test/testDomain.py
@@ -104,5 +104,58 @@ class DomainTest(unittest.TestCase):
         self.downloadMock.getStockHistoricalData.assert_called_once_with(initialDate, today, quote)
         self.mongoMock.saveStockHistoricalData.assert_called_once_with(quote, stockHistoricalDataArray)
 
+    def testDownloadAndSaveStockHistoricalData_OK(self):
+        quote = "BAC"
+        stock = { "name": "Bank of America", "quote": quote, "stockMarket": "NYSE" }
+        stockHistoricalDataArray = [
+            {
+                "Adj_Close": "13.79",
+                "Date": "2016-03-18",
+                "Close": "13.79",
+                "Volume": "145037500",
+                "Open": "13.68",
+                "Low": "13.55",
+                "High": "13.88",
+                "Symbol": "BAC"
+            },
+            {
+                "Adj_Close": "13.40",
+                "Date": "2016-03-17",
+                "Close": "13.40",
+                "Volume": "121732700",
+                "Open": "13.22",
+                "Low": "13.05",
+                "High": "13.48",
+                "Symbol": "BAC"
+            },
+            {
+                "Adj_Close": "13.31",
+                "Date": "2016-03-16",
+                "Close": "13.31",
+                "Volume": "148489100",
+                "Open": "13.51",
+                "Low": "13.09",
+                "High": "13.81",
+                "Symbol": "BAC"
+            }
+        ]
+        self.downloadMock.getStockHistoricalData = Mock(return_value=stockHistoricalDataArray)
+        self.mongoMock.saveStockHistoricalData = Mock()
+        self.domain.downloadAndSaveStockHistoricalData(stock)
+        self.assertTrue(self.downloadMock.getStockHistoricalData.call_count == self.domain.YEARS_AGO)
+        self.mongoMock.saveStockHistoricalData.assert_called_with(quote, stockHistoricalDataArray)
+        self.assertTrue(self.mongoMock.saveStockHistoricalData.call_count == self.domain.YEARS_AGO)
+
+    def testDownloadAndSaveStockHistoricalData_NOK_emptyStockHistoricalDataArray(self):
+        quote = "BAC"
+        stock = { "name": "Bank of America", "quote": quote, "stockMarket": "NYSE" }
+        stockHistoricalDataArray = []
+        self.downloadMock.getStockHistoricalData = Mock(return_value=stockHistoricalDataArray)
+        self.mongoMock.saveStockHistoricalData = Mock()
+        self.domain.downloadAndSaveStockHistoricalData(stock)
+        self.assertTrue(self.downloadMock.getStockHistoricalData.call_count == self.domain.YEARS_AGO)
+        self.mongoMock.saveStockHistoricalData.assert_called_with(quote, stockHistoricalDataArray)
+        self.assertTrue(self.mongoMock.saveStockHistoricalData.call_count == self.domain.YEARS_AGO)
+
 if __name__ == "main":
     unittest.main()
