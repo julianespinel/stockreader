9ace6d833ce38bfb06e39ec53209ec0f3e8ed44f
Julian Espinel <julianespinel@gmail.com>
2016-04-26 22:24:18 -0500

    Make job.py a class.

--- src/job.py
+++ src/job.py
@@ -1,41 +1,45 @@
 import time
 import schedule
-import mongo
-import domain
 from concurrent.futures import ThreadPoolExecutor
 
-WORKERS = 8
-DAYS_FROM_TODAY = 7
+class Job:
 
-def downloadAndSaveStockCurrentDataInParallel():
-    stocks = mongo.readStocksFromStockList()
-    with ThreadPoolExecutor(max_workers=WORKERS) as executor:
-        for stock in stocks:
-            executor.submit(domain.downloadAndSaveStockCurrentData, stock)
+    WORKERS = 8
+    DAYS_FROM_TODAY = 7
 
-def downloadAndSaveStockWeeklyDataInParallel():
-    stocks = mongo.readStocksFromStockList()
-    with ThreadPoolExecutor(max_workers=WORKERS) as executor:
-        for stock in stocks:
-            executor.submit(domain.downloadAndSaveStockDataDaysFromToday, stock, DAYS_FROM_TODAY)
+    def __init__(self, mongo, domain):
+        self.mongo = mongo
+        self.domain = domain
 
-def downloadAndSaveStockHistoricalDataInParallel():
-    stocks = mongo.readStocksFromStockList()
-    with ThreadPoolExecutor(max_workers=WORKERS) as executor:
-        for stock in stocks:
-            executor.submit(domain.downloadAndSaveStockHistoricalData, stock)
+    def downloadAndSaveStockCurrentDataInParallel(self):
+        stocks = self.mongo.readStocksFromStockList()
+        with ThreadPoolExecutor(max_workers=self.WORKERS) as executor:
+            for stock in stocks:
+                executor.submit(self.domain.downloadAndSaveStockCurrentData, stock)
 
-def updateStocks():
-    schedule.every(1).hour.do(downloadAndSaveStockCurrentDataInParallel)
-    schedule.every().day.at("18:00").do(downloadAndSaveStockWeeklyDataInParallel)
-    schedule.every().saturday.at("23:00").do(downloadAndSaveStockHistoricalDataInParallel)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    def downloadAndSaveStockWeeklyDataInParallel(self):
+        stocks = self.mongo.readStocksFromStockList()
+        with ThreadPoolExecutor(max_workers=self.WORKERS) as executor:
+            for stock in stocks:
+                executor.submit(self.domain.downloadAndSaveStockDataDaysFromToday, stock, self.DAYS_FROM_TODAY)
 
-def addStockToStockboard(stock):
-    mongo.saveStockList([stock])
-    with ThreadPoolExecutor(max_workers=WORKERS) as executor:
-        executor.submit(domain.downloadAndSaveStockCurrentData, stock)
-        executor.submit(domain.downloadAndSaveStockDataDaysFromToday, stock, DAYS_FROM_TODAY)
-        executor.submit(domain.downloadAndSaveStockHistoricalData, stock)
+    def downloadAndSaveStockHistoricalDataInParallel(self):
+        stocks = self.mongo.readStocksFromStockList()
+        with ThreadPoolExecutor(max_workers=self.WORKERS) as executor:
+            for stock in stocks:
+                executor.submit(self.domain.downloadAndSaveStockHistoricalData, stock)
+
+    def updateStocks(self):
+        schedule.every(1).hour.do(self.downloadAndSaveStockCurrentDataInParallel)
+        schedule.every().day.at("18:00").do(self.downloadAndSaveStockWeeklyDataInParallel)
+        schedule.every().saturday.at("23:00").do(self.downloadAndSaveStockHistoricalDataInParallel)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    def addStockToStockboard(self, stock):
+        self.mongo.saveStockList([stock])
+        with ThreadPoolExecutor(max_workers=self.WORKERS) as executor:
+            executor.submit(self.domain.downloadAndSaveStockCurrentData, stock)
+            executor.submit(self.domain.downloadAndSaveStockDataDaysFromToday, stock, self.DAYS_FROM_TODAY)
+            executor.submit(self.domain.downloadAndSaveStockHistoricalData, stock)
