50077dd8decf6a442d007d11f0af164972ea390a
Julian Espinel <julianespinel@gmail.com>
2016-07-28 08:24:09 -0500

    Add more unit test cases to the method post() from the StockAPI class.

--- test/api_test.py
+++ test/api_test.py
@@ -21,9 +21,40 @@ class ApiTest(unittest.TestCase):
         flaskApi.init_app(app)
         self.client = app.test_client()
 
-    def testAddStock_NOK_badRequest(self):
+    def testAddStock_NOK_emptyRequestBody(self):
         response = self.client.post("/stockboard/api/stocks")
         self.assertEquals(response.status_code, 400)
         data = json.loads(response.data)
-        expectedErrorMessage = "Please provide a valid stock. It should contain a name, a quote and a stock market"
+        expectedErrorMessage = "Please provide a stock in the request body. It should have a name, a quote and a stock market"
         self.assertEquals(expectedErrorMessage, data["error"])
+
+    def testAddStock_NOK_notValidStock(self):
+        stock = { "name": "Bank of America", "quote": "", "stockMarket": "" }
+        response = self.client.post("/stockboard/api/stocks", data=json.dumps(stock), content_type="application/json")
+        self.assertEquals(response.status_code, 400)
+        data = json.loads(response.data)
+        expectedErrorMessage = "Please provide a valid stock. It should have a name, a quote and a stock market"
+        self.assertEquals(expectedErrorMessage, data["error"])
+
+    def testAddStock_NOK_existingStock(self):
+        self.mongoMock.getStockByQuote = Mock(return_value=True)
+        quote = "BAC"
+        stock = { "name": "Bank of America", "quote": quote, "stockMarket": "NYSE" }
+        response = self.client.post("/stockboard/api/stocks", data=json.dumps(stock), content_type="application/json")
+        self.mongoMock.getStockByQuote.assert_called_once_with(quote)
+        self.assertEquals(response.status_code, 409)
+        data = json.loads(response.data)
+        expectedErrorMessage = "The given stock already exists"
+        self.assertEquals(expectedErrorMessage, data["error"])
+
+    def testAddStock_OK(self):
+        self.mongoMock.getStockByQuote = Mock(return_value=False)
+        quote = "BAC"
+        stock = { "name": "Bank of America", "quote": quote, "stockMarket": "NYSE" }
+        response = self.client.post("/stockboard/api/stocks", data=json.dumps(stock), content_type="application/json")
+        self.mongoMock.getStockByQuote.assert_called_once_with(quote)
+        self.assertEquals(response.status_code, 202)
+        data = json.loads(response.data)
+        expectedMessage = "The stock " + quote + " is being added"
+        self.assertEquals(expectedMessage, data["success"])
+
