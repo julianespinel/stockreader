c32ba97d39c6fc93e8ce2ded51b7e7990f0186b0
Julian Espinel <julianespinel@gmail.com>
2016-08-01 13:53:23 -0500

    Add unit tests to the Download class.

---
+++ test/download_test.py
@@ -0,0 +1,47 @@
+import datetime
+import unittest
+
+from .context import src
+from src import download
+
+class DownloadTest(unittest.TestCase):
+
+    def setUp(self):
+        self.download = download.Download()
+
+    def testGetStockHistoricalData_OK(self):
+        initialDate = datetime.date(2016, 7, 1)
+        finalDate = datetime.date(2016, 7, 30)
+        quote = "BAC"
+        stockHistoricalDataArray = self.download.getStockHistoricalData(initialDate, finalDate, quote)
+        self.assertIsNot(0, len(stockHistoricalDataArray))
+        historicalData = stockHistoricalDataArray[0]
+        # Check one element structure
+        self.assertIsNotNone(historicalData["Symbol"])
+        self.assertIsNotNone(historicalData["Date"])
+        self.assertIsNotNone(historicalData["Open"])
+        self.assertIsNotNone(historicalData["High"])
+        self.assertIsNotNone(historicalData["Low"])
+        self.assertIsNotNone(historicalData["Close"])
+        self.assertIsNotNone(historicalData["Volume"])
+        self.assertIsNotNone(historicalData["Adj_Close"])
+
+    def testGetStockCurrentData_OK(self):
+        quote = "BAC"
+        stockCurrentData = self.download.getStockCurrentData(quote)
+        self.assertIsNotNone(stockCurrentData)
+        # Check current data structure
+        self.assertIsNotNone(stockCurrentData["symbol"])
+        self.assertIsNotNone(stockCurrentData["AverageDailyVolume"])
+        self.assertIsNotNone(stockCurrentData["Change"])
+        self.assertIsNotNone(stockCurrentData["DaysLow"])
+        self.assertIsNotNone(stockCurrentData["DaysHigh"])
+        self.assertIsNotNone(stockCurrentData["YearLow"])
+        self.assertIsNotNone(stockCurrentData["YearHigh"])
+        self.assertIsNotNone(stockCurrentData["MarketCapitalization"])
+        self.assertIsNotNone(stockCurrentData["LastTradePriceOnly"])
+        self.assertIsNotNone(stockCurrentData["DaysRange"])
+        self.assertIsNotNone(stockCurrentData["Name"])
+        self.assertIsNotNone(stockCurrentData["Symbol"])
+        self.assertIsNotNone(stockCurrentData["Volume"])
+        self.assertIsNotNone(stockCurrentData["StockExchange"])
