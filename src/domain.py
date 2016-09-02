63128dcd117b92bb196cd4cc494623520aa891f9
Julian Espinel <julianespinel@gmail.com>
2016-09-01 21:05:20 -0500

    Simplify Domain class.

--- src/domain.py
+++ src/domain.py
@@ -5,8 +5,6 @@ logger = log.getLogger("domain")
 
 class Domain:
 
-    YEARS_AGO = 10
-
     def __init__(self, mongo, download):
         self.mongo = mongo
         self.download = download
@@ -17,23 +15,10 @@ class Domain:
         stockCurrentData = self.download.getStockCurrentData(quote)
         self.mongo.upsertStockCurrentData(quote, stockCurrentData)
 
-    def downloadAndSaveStockDataDaysFromToday(self, stock, daysFromToday):
-        today = date.today()
-        initialDate = today - timedelta(days=daysFromToday)
+    def downloadAndSaveStockHistoricalData(self, initialDate, finalDate, stock):
         quote = stock["quote"]
-        logger.info("stock %s initialDate %s today %s", quote, initialDate, today)
-        stockHistoricalDataArray = self.download.getStockHistoricalData(initialDate, today, quote)
+        stockHistoricalDataArray = self.download.getStockHistoricalData(initialDate, finalDate, quote)
         self.mongo.saveStockHistoricalData(quote, stockHistoricalDataArray)
-
-    def downloadAndSaveStockHistoricalData(self, stock):
-        today = date.today()
-        quote = stock["quote"]
-        for index in range(self.YEARS_AGO):
-            initialDate = today.replace(year=(today.year-(index+1)))
-            finalDate = today.replace(year=(today.year-index))
-            logger.info("stock %s initialDate %s finalDate %s", quote, initialDate, finalDate)
-            stockHistoricalDataArray = self.download.getStockHistoricalData(initialDate, finalDate, quote)
-            self.mongo.saveStockHistoricalData(quote, stockHistoricalDataArray)
 
     def stockExists(self, quote):
         return self.mongo.stockExists(quote)
