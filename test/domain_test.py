3d22f3229de261c6813b0d656c7d486576d2f85c
Julian Espinel <julianespinel@gmail.com>
2016-09-03 20:23:08 -0500

    Use factories instead of hard coded values in domain_test.py

--- test/domain_test.py
+++ test/domain_test.py
@@ -1,4 +1,6 @@
 import unittest
+import test.factories as factories
+
 from datetime import date
 from unittest.mock import Mock
 
@@ -13,24 +15,9 @@ class DomainTest(unittest.TestCase):
         self.domain = domain.Domain(self.mongoMock, self.downloadMock)
 
     def testDownloadAndSaveStockCurrentData_OK(self):
-        quote = "BAC"
-        stock = { "name": "Bank of America", "symbol": quote, "stockMarket": "NYSE" }
-        stockCurrentData = {
-            "symbol" : "BAC",
-            "DaysLow" : "13.780",
-            "StockExchange" : "NYQ",
-            "DaysRange" : "13.780 - 13.940",
-            "Volume" : "3255146",
-            "LastTradePriceOnly" : "13.925",
-            "YearHigh" : "18.480",
-            "MarketCapitalization" : "146.44B",
-            "Name" : "Bank of America Corporation Com",
-            "AverageDailyVolume" : "136399008",
-            "Change" : "+0.135",
-            "DaysHigh" : "13.940",
-            "Symbol" : "BAC",
-            "YearLow" : "10.990"
-        }
+        stock = factories.getStockData()
+        quote = stock["symbol"]
+        stockCurrentData = factories.getStockCurrentData()
         self.downloadMock.getStockCurrentData = Mock(return_value=stockCurrentData)
         self.mongoMock.upsertStockCurrentData = Mock()
         self.domain.downloadAndSaveStockCurrentData(stock)
@@ -38,8 +25,8 @@ class DomainTest(unittest.TestCase):
         self.mongoMock.upsertStockCurrentData.assert_called_once_with(quote, stockCurrentData)
 
     def testDownloadAndSaveStockCurrentData_NOK_emptyStockCurrentData(self):
-        quote = "BAC"
-        stock = { "name": "Bank of America", "symbol": quote, "stockMarket": "NYSE" }
+        stock = factories.getStockData()
+        quote = stock["symbol"]
         stockCurrentData = None
         self.downloadMock.getStockCurrentData = Mock(return_value=stockCurrentData)
         self.mongoMock.upsertStockCurrentData = Mock()
@@ -50,40 +37,9 @@ class DomainTest(unittest.TestCase):
     def testDownloadAndSaveStockHistoricalData_OK(self):
         initialDate = date(2006, 9, 1)
         finalDate = date(2016, 9, 1)
-        quote = "BAC"
-        stock = { "name": "Bank of America", "symbol": quote, "stockMarket": "NYSE" }
-        stockHistoricalDataArray = [
-            {
-                "Adj_Close": "13.79",
-                "Date": "2016-03-18",
-                "Close": "13.79",
-                "Volume": "145037500",
-                "Open": "13.68",
-                "Low": "13.55",
-                "High": "13.88",
-                "Symbol": "BAC"
-            },
-            {
-                "Adj_Close": "13.40",
-                "Date": "2016-03-17",
-                "Close": "13.40",
-                "Volume": "121732700",
-                "Open": "13.22",
-                "Low": "13.05",
-                "High": "13.48",
-                "Symbol": "BAC"
-            },
-            {
-                "Adj_Close": "13.31",
-                "Date": "2016-03-16",
-                "Close": "13.31",
-                "Volume": "148489100",
-                "Open": "13.51",
-                "Low": "13.09",
-                "High": "13.81",
-                "Symbol": "BAC"
-            }
-        ]
+        stock = factories.getStockData()
+        quote = stock["symbol"]
+        stockHistoricalDataArray = factories.getStockHistoricalDataArray()
         self.downloadMock.getStockHistoricalData = Mock(return_value=stockHistoricalDataArray)
         self.mongoMock.saveStockHistoricalData = Mock()
         self.domain.downloadAndSaveStockHistoricalData(initialDate, finalDate, stock)
@@ -93,8 +49,8 @@ class DomainTest(unittest.TestCase):
     def testDownloadAndSaveStockHistoricalData_NOK_emptyStockHistoricalDataArray(self):
         initialDate = date(2006, 9, 1)
         finalDate = date(2016, 9, 1)
-        quote = "BAC"
-        stock = { "name": "Bank of America", "symbol": quote, "stockMarket": "NYSE" }
+        stock = factories.getStockData()
+        quote = stock["symbol"]
         stockHistoricalDataArray = []
         self.downloadMock.getStockHistoricalData = Mock(return_value=stockHistoricalDataArray)
         self.mongoMock.saveStockHistoricalData = Mock()
@@ -117,11 +73,7 @@ class DomainTest(unittest.TestCase):
         self.mongoMock.stockExists.assert_called_once_with(quote)
 
     def testGetStockList_OK(self):
-        expectedStockList = [
-            { "stockMarket" : "nyse", "name" : "Valspar", "symbol" : "VAL" },
-            { "stockMarket" : "nyse", "name" : "Trinseo", "symbol" : "TSE" },
-            { "stockMarket" : "nyse", "name" : "Celestica", "symbol" : "CLS" }
-        ]
+        expectedStockList = factories.getStockList()
         self.mongoMock.readStocksFromStockList = Mock(return_value=expectedStockList)
         stockList = self.domain.getStockList()
         self.assertEqual(len(expectedStockList), len(stockList))
@@ -135,10 +87,7 @@ class DomainTest(unittest.TestCase):
         self.mongoMock.readStocksFromStockList.assert_called_once_with()
 
     def testAddStockToStockList_OK(self):
-        stock = { "stockMarket" : "nyse", "name" : "Trinseo", "symbol" : "TSE" }
+        stock = factories.getStockData()
         self.mongoMock.saveStockList = Mock()
         self.domain.addStockToStockList(stock)
         self.mongoMock.saveStockList.assert_called_once_with([stock])
-
-if __name__ == "main":
-    unittest.main()
