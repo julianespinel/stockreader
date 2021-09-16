5290bc52d6e02550b46602299e8b4c777efcdca7
Julian Espinel <julianespinel@gmail.com>
2021-09-15 20:30:29 -0400

    Read and print stocks from CSV file

--- v2/stockreader/app/Main.hs
+++ v2/stockreader/app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
 import Lib
+import System.Environment
 
+-- How to run?
+-- stack run "resources/US_LIST_OF_SYMBOLS.csv"
 main :: IO ()
-main = sayHelloToAll
+main = do
+    args <- getArgs
+    printStocks $ head args
