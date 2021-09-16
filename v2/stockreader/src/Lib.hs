2213285a14d66222e30d40f4b7872e0b8f69717e
Julian Espinel <julianespinel@gmail.com>
2021-09-15 20:29:04 -0400

    Read, filter, and print CSV stocks

--- v2/stockreader/src/Lib.hs
+++ v2/stockreader/src/Lib.hs
@@ -1,22 +1,57 @@
-module Lib (sayHelloToAll) where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Data.Char (isSpace)
-import System.Environment
-import Text.Printf
+module Lib (printStocks) where
 
-isValid :: String -> Bool
-isValid name = not (null name || all isSpace name)
+import Control.Monad
+import qualified Data.ByteString.Lazy as BL
+import Data.Csv
+import qualified Data.Vector as V
 
-sayHello :: String -> String
-sayHello = printf "Hello %s from Haskell!"
+-- data type to model a stock
+data Stock = Stock
+  { code :: String,
+    name :: String,
+    country :: String,
+    exchange :: String,
+    currency :: String,
+    instrumentType :: String
+  }
+  deriving (Show)
 
-printAll :: [String] -> IO ()
-printAll = mapM_ print
+instance FromNamedRecord Stock where
+  parseNamedRecord record =
+    Stock
+      <$> record .: "Code"
+      <*> record .: "Name"
+      <*> record .: "Country"
+      <*> record .: "Exchange"
+      <*> record .: "Currency"
+      <*> record .: "Type"
 
-sayHelloToAll :: IO ()
-sayHelloToAll = do
-  print "start"
-  args <- getArgs
-  --   stocks <- downloadStocksToAnalyse
-  printAll $ (map sayHello . filter isValid) args
-  print "end"
+-- type synonyms to handle the CSV contents
+type ErrorMsg = String
+
+type CsvData = (Header, V.Vector Stock)
+
+-- Function to read the CSV
+parseCSV :: FilePath -> IO (Either ErrorMsg CsvData)
+parseCSV filePath = do
+  contents <- BL.readFile filePath
+  return $ decodeByName contents
+
+-- Discard headers from CsvData
+removeHeaders :: CsvData -> V.Vector Stock
+removeHeaders = snd
+
+-- Check if the given element is a Common Stock
+isStock :: Stock -> Bool
+isStock stock = instrumentType stock == "Common Stock"
+
+filterStocks :: V.Vector Stock -> V.Vector Stock
+filterStocks = V.filter isStock
+
+-- Print the stocks from the CSV file
+printStocks :: FilePath -> IO ()
+printStocks filePath =
+  parseCSV filePath
+    >>= print . fmap (filterStocks . removeHeaders)
