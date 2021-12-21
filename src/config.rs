263dea38bd234a5589d31b270e6a6bbea02abac2
Julian Espinel <julianespinel@gmail.com>
2021-12-21 18:05:36 -0500

    Get database url from config file

--- src/config.rs
+++ src/config.rs
@@ -4,14 +4,44 @@ use std::io::Error;
 use serde::Deserialize;
 
 #[derive(Default, Deserialize)]
-pub struct Configuration {
+pub struct IEXConfig {
     pub environment: String,
     pub version: String,
     pub api_key: String,
+}
+
+#[derive(Default, Deserialize)]
+pub struct DatabaseConfig {
+    pub host: String,
+    pub username: String,
+    pub password: String,
+    pub port: String,
+    pub name: String,
+}
+
+#[derive(Default, Deserialize)]
+pub struct Configuration {
+    pub iex: IEXConfig,
+    pub database: DatabaseConfig,
 }
 
 pub fn read_config(config_path: &str) -> Result<Configuration, Error> {
     let content = read_to_string(config_path)?;
     let config = toml::from_str(&content)?;
     Ok(config)
+}
+
+pub fn get_iex_host(iex_config: &IEXConfig) -> String {
+    format!("https://{}.iexapis.com/{}", iex_config.environment, iex_config.version)
+}
+
+pub fn get_database_url(db_config: DatabaseConfig) -> String {
+    format!(
+        "postgres://{}:{}@{}:{}/{}",
+        db_config.username,
+        db_config.password,
+        db_config.host,
+        db_config.port,
+        db_config.name
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,10 @@ mod schema;
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = config::read_config("config-dev.toml")?;
-    let host = format!("https://{}.iexapis.com/{}", config.environment, config.version);
+    let host = config::get_iex_host(&config.iex);
+    let db_url = config::get_database_url(config.database);
 
-    let iex_client = client::IEXClient { host: &host, api_key: &config.api_key };
+    let iex_client = client::IEXClient { host: &host, api_key: &config.iex.api_key };
     let symbols = iex_client.get_symbols().await?;
     repository::save_symbols(&db_url, symbols)?;
     Ok(())
