a3db0cf0ab760a10531c89a618586bc13f2ec802
Julian Espinel <julianespinel@gmail.com>
2021-12-21 22:39:39 -0500

    Log using log4rs implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate diesel;
 
+use log::{info};
+use log4rs;
+
 use anyhow::Result;
 
 mod client;
@@ -11,9 +14,14 @@ mod schema;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    log4rs::init_file("log4rs.yaml", Default::default())
+        .expect("Could not initialize log4rs logger");
+
+    info!("start");
     let config = config::read_config("config-dev.toml")?;
     let host = config::get_iex_host(&config.iex);
     let db_url = config::get_database_url(config.database);
+    info!("configuration was read");
 
     let iex_client = client::IEXClient { host: &host, api_key: &config.iex.api_key };
     let symbols = iex_client.get_symbols().await?;
