c0286e7657cac59ee9c4e35e9e8202828c15db6b
Julian Espinel <julianespinel@gmail.com>
2021-12-26 10:25:13 -0500

    Add IEXClient success test case

--- src/client.rs
+++ src/client.rs
@@ -6,10 +6,47 @@ pub(super) struct IEXClient<'a> {
 }
 
 impl<'a> IEXClient<'a> {
-
     pub async fn get_symbols(&self) -> Result<Vec<Symbol>, reqwest::Error> {
         let url = format!("{}/ref-data/symbols?token={}", self.host, self.api_key);
         let symbols = reqwest::get(&url).await?.json::<Vec<Symbol>>().await?;
         Ok(symbols)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use httpmock::Method::GET;
+    use httpmock::MockServer;
+
+    use crate::client::IEXClient;
+
+//-------------------------------------------------------------------------
+    // get_iex_host tests
+    //-------------------------------------------------------------------------
+
+
+    #[tokio::test]
+    async fn get_symbols_returns_200() {
+        // arrange
+        let server = MockServer::start();
+
+        let api_key = "ak";
+        let host = server.base_url();
+        let client = IEXClient { api_key, host: &host };
+
+        let get_symbols_mock = server.mock(|when, then| {
+            when.method(GET)
+                .path("/ref-data/symbols")
+                .query_param("token", client.api_key);
+            then.status(200)
+                .header("content-type", "application/json")
+                .body_from_file("tests/httpmock_files/symbols.json");
+        });
+
+        // act
+        let symbols = client.get_symbols().await.unwrap();
+        // assert
+        get_symbols_mock.assert();
+        assert_eq!(symbols.len(), 3);
     }
 }
