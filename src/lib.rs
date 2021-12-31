8bb604ba9e2e76947b4c49f6f5164050de0d1543
Julian Espinel <julianespinel@gmail.com>
2021-12-30 20:20:06 -0500

    Pass config file as param to make it easier to test

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,19 @@ use log::info;
 
 use crate::service::Service;
 
+pub mod config;
+
 mod client;
 mod models;
 mod repository;
-mod config;
 mod schema;
 mod service;
 
-pub async fn download_symbols() -> Result<()> {
-    let config = config::read_config("config-dev.toml")?;
+pub async fn download_symbols(config_file_path: &str) -> Result<()> {
+    let config = config::read_config(config_file_path)?;
     let host = config::get_iex_host(&config.iex);
     let db_url = config::get_database_url(&config.database);
     info!("configuration was read");
-
-
 
     let iex_client = client::IEXClient::new(&config.iex.api_key, &host);
     let repository = repository::Repository { db_url: &db_url };
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() -> Result<()> {
     log4rs::init_file("log4rs.yaml", Default::default())
         .expect("Could not initialize log4rs logger");
     info!("start");
-    stockreader::download_symbols().await?;
+    stockreader::download_symbols("config-dev.toml").await?;
     info!("done");
     Ok(())
 }
