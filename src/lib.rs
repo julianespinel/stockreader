7802adb0f363ea17b46c760fd84a49aa2c258e82
Julian Espinel <julianespinel@gmail.com>
2022-01-07 19:07:26 -0500

    Receive configuration instead of file_path

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate diesel;
 
 use anyhow::Result;
 use log::info;
+use crate::config::models::Configuration;
 
 use crate::service::Service;
 
@@ -14,19 +15,16 @@ mod repository;
 mod schema;
 mod service;
 
-pub async fn download_symbols(config_file_path: &str) -> Result<()> {
-    let config = config::read_config(config_file_path)?;
-    let host = config::get_iex_host(&config.iex);
-    let db_url = config::get_database_url(&config.database);
-    info!("configuration was read");
+pub async fn download_symbols(config: &Configuration) -> Result<()> {
+    let host = config.iex.get_host();
+    let db_url = config.database.get_url();
 
     let iex_client = client::IEXClient::new(&config.iex.api_key, &host);
     let repository = repository::Repository { db_url: &db_url };
     let service = Service { iex_client: &iex_client, repository: &repository };
-    info!("startup is done");
 
     info!("start: getting symbols");
-    service.get_symbols().await?;
+    service.download_symbols().await?;
     info!("done: getting symbols");
     Ok(())
 }
