6214e21cd7975143ebcef3d3b355edfbcf1c008d
Julian Espinel <julianespinel@gmail.com>
2022-01-20 22:41:20 -0500

    Add tests for 'new' and 'save_symbols'

--- src/repository.rs
+++ src/repository.rs
@@ -6,8 +6,8 @@ use log::debug;
 use reqwest::Url;
 
 use crate::models::{Stats, Symbol};
-use crate::schema::{stats, symbols};
 use crate::schema::symbols::dsl::*;
+use crate::schema::{stats, symbols};
 
 pub(super) struct Repository<'a> {
     pub db_url: &'a str,
@@ -17,14 +17,15 @@ impl<'a> Repository<'a> {
     // private functions
 
     fn get_connection(&self) -> PgConnection {
-        PgConnection::establish(self.db_url)
-            .expect(&format!("Error connecting to {}", self.db_url))
+        PgConnection::establish(self.db_url).expect(&format!("Error connecting to {}", self.db_url))
     }
 
     // public functions
 
-    pub fn new (db_url: &'a str) -> Repository<'a> {
-        if db_url.is_empty() { panic!("db_url must not be empty") }
+    pub fn new(db_url: &'a str) -> Repository<'a> {
+        if db_url.is_empty() {
+            panic!("db_url must not be empty")
+        }
         Url::parse(db_url).expect(&format!("db_url: '{}' is not valid", db_url));
         Repository { db_url }
     }
@@ -47,9 +48,7 @@ impl<'a> Repository<'a> {
 
     pub fn save_stats(&self, new_stats: Vec<Stats>) -> Result<(), Error> {
         let conn = self.get_connection();
-        let affected_rows = insert_into(stats::table)
-            .values(new_stats)
-            .execute(&conn)?;
+        let affected_rows = insert_into(stats::table).values(new_stats).execute(&conn)?;
         debug!("saved {} stats in DB", affected_rows);
         Ok(())
     }
@@ -57,12 +56,16 @@ impl<'a> Repository<'a> {
 
 #[cfg(test)]
 mod tests {
-//-------------------------------------------------------------------------
-// new() tests
-//-------------------------------------------------------------------------
+    use testcontainers::clients;
 
     use crate::config::models::DatabaseConfig;
     use crate::repository::Repository;
+
+    const POSTGRES_CONTAINER_PORT: u16 = 5432;
+
+    //-------------------------------------------------------------------------
+    // new() tests
+    //-------------------------------------------------------------------------
 
     #[test]
     #[should_panic(expected = "db_url must not be empty")]
@@ -85,6 +88,15 @@ mod tests {
     #[test]
     fn new_given_valid_db_url_returns_repository() {
         // arrange
+        let db_config = get_db_config_for_tests();
+        let db_url = db_config.get_url();
+        // act
+        let repository = Repository::new(&db_url);
+        // assert
+        assert_eq!(repository.db_url, db_url);
+    }
+
+    fn get_db_config_for_tests() -> DatabaseConfig {
         let db_config = DatabaseConfig {
             username: "username".to_string(),
             password: "password".to_string(),
@@ -92,10 +104,133 @@ mod tests {
             port: 5432,
             name: "some_db".to_string(),
         };
-        let db_url = db_config.get_url();
-        // act
+        db_config
+    }
+
+    //-------------------------------------------------------------------------
+    // save_symbols tests
+    //-------------------------------------------------------------------------
+
+    #[test]
+    fn save_symbols_given_empty_list_saves_nothing() {
+        // arrange
+        let db_config = get_db_config_for_tests();
+        let docker = clients::Cli::default();
+        let container = containers::start_postgres_container(&db_config, &docker);
+        let host_port = container.get_host_port(POSTGRES_CONTAINER_PORT).unwrap();
+        let db_url = containers::get_test_db_url(&db_config, &host_port);
+
+        containers::execute_db_migrations(&db_url);
         let repository = Repository::new(&db_url);
+        let symbols = vec![];
+        // act
+        repository
+            .save_symbols(&symbols)
+            .expect("error saving symbols");
         // assert
-        assert_eq!(repository.db_url, db_url);
+        let symbols_from_db = repository.get_symbols().expect("error getting symbols");
+        assert!(symbols_from_db.is_empty());
+    }
+
+    #[test]
+    fn save_symbols_given_non_empty_list_saves_symbols() {
+        // arrange
+        let db_config = get_db_config_for_tests();
+        let docker = clients::Cli::default();
+        let container = containers::start_postgres_container(&db_config, &docker);
+        let host_port = container.get_host_port(POSTGRES_CONTAINER_PORT).unwrap();
+        let db_url = containers::get_test_db_url(&db_config, &host_port);
+
+        containers::execute_db_migrations(&db_url);
+        let repository = Repository::new(&db_url);
+        let list_size = 5;
+        let symbols = test_factories::get_symbols_list(list_size);
+        // act
+        repository
+            .save_symbols(&symbols)
+            .expect("error saving symbols");
+        // assert
+        let symbols_from_db = repository.get_symbols().expect("error getting symbols");
+        assert_eq!(symbols_from_db.len(), list_size as usize);
+    }
+
+    /// Module for using containers in tests
+    mod containers {
+        use diesel::{Connection, PgConnection};
+        use testcontainers::clients::Cli;
+        use testcontainers::images::generic::{GenericImage, WaitFor};
+        use testcontainers::{images, Container, Docker};
+
+        use crate::config::models::DatabaseConfig;
+        use crate::embedded_migrations;
+
+        pub(super) fn get_test_db_url<'a>(
+            db_config: &'a DatabaseConfig,
+            host_port: &'a u16,
+        ) -> String {
+            let db_url = format!(
+                "postgres://{}:{}@{}:{}/{}",
+                db_config.username, db_config.password, db_config.host, host_port, db_config.name
+            );
+            db_url
+        }
+
+        pub(super) fn execute_db_migrations(db_url: &str) {
+            let conn = PgConnection::establish(db_url)
+                .expect(&format!("Cannot connect to database: {}", db_url));
+
+            embedded_migrations::run(&conn).expect("error running diesel migrations");
+        }
+
+        pub(super) fn start_postgres_container<'a>(
+            db_config: &DatabaseConfig,
+            docker: &'a Cli,
+        ) -> Container<'a, Cli, GenericImage> {
+            let generic_postgres = images::generic::GenericImage::new("postgres")
+                .with_wait_for(WaitFor::message_on_stderr(
+                    "database system is ready to accept connections",
+                ))
+                .with_env_var("POSTGRES_USER", &db_config.username)
+                .with_env_var("POSTGRES_PASSWORD", &db_config.password)
+                .with_env_var("POSTGRES_DB", &db_config.name);
+
+            let container = docker.run(generic_postgres);
+            container
+        }
+    }
+
+    mod test_factories {
+        use std::iter;
+
+        use rand::distributions::Alphanumeric;
+        use rand::{thread_rng, Rng};
+
+        use crate::models::Symbol;
+
+        fn get_random_string() -> String {
+            let mut rng = thread_rng();
+            let random_string: String = iter::repeat(())
+                .map(|()| rng.sample(Alphanumeric))
+                .map(char::from)
+                .take(7)
+                .collect();
+            random_string
+        }
+
+        pub(super) fn get_random_symbol(index: u32) -> Symbol {
+            let random_string = get_random_string();
+            let symbol = format!("{}{}", random_string.clone(), index);
+            let name = format!("{}-name", &symbol);
+            Symbol { symbol, name }
+        }
+
+        pub(super) fn get_symbols_list(quantity: u32) -> Vec<Symbol> {
+            let mut symbols = vec![];
+            for i in 0..quantity {
+                let symbol = get_random_symbol(i);
+                symbols.push(symbol);
+            }
+            symbols
+        }
     }
 }
