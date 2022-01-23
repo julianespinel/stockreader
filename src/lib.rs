d0daad066a9dd35be600ec2cb51e767279075239
Julian Espinel <julianespinel@gmail.com>
2022-01-23 12:33:22 -0500

    Add logic to download stock stats

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 extern crate diesel;
 #[macro_use]
 extern crate diesel_migrations;
+#[macro_use]
+extern crate bigdecimal;
 
 use anyhow::Result;
 use diesel::{Connection, PgConnection};
@@ -25,6 +27,7 @@ pub async fn execute(action: &str, config: &Configuration) -> Result<()> {
     match action {
         "migrate" => run_db_migrations(config)?,
         "download_symbols" => download_symbols(config).await?,
+        "download_stats" => download_stats(config).await?,
         _ => warn!("Unknown action: {}", action)
     }
     Ok(())
@@ -49,5 +52,19 @@ pub async fn download_symbols(config: &Configuration) -> Result<()> {
     info!("start: getting symbols");
     service.download_symbols().await?;
     info!("done: getting symbols");
+    Ok(())
+}
+
+pub async fn download_stats(config: &Configuration) -> Result<()> {
+    let base_url = &config.iex.base_url;
+    let db_url = config.database.get_url();
+
+    let iex_client = client::IEXClient::new(&config.iex.api_key, base_url);
+    let repository = repository::Repository::new(&db_url);
+    let service = Service { iex_client: &iex_client, repository: &repository };
+
+    info!("start: getting stats");
+    service.download_stats().await?;
+    info!("done: getting stats");
     Ok(())
 }
