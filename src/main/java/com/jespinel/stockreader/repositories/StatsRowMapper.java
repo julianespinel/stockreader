298ea0b74161e1061804e26f14d4afc25f5b5a39
Julian Espinel <julianespinel@gmail.com>
2022-01-31 19:12:07 -0500

    Move getDate to RowMapperUtils to re-use it

--- src/main/java/com/jespinel/stockreader/repositories/StatsRowMapper.java
+++ src/main/java/com/jespinel/stockreader/repositories/StatsRowMapper.java
@@ -5,7 +5,6 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -23,9 +22,9 @@ public class StatsRowMapper implements RowMapper<Stats> {
         BigDecimal ttmDividendRate = BigDecimal.valueOf(rs.getLong("ttm_dividend_rate"));
         BigDecimal dividendYield = BigDecimal.valueOf(rs.getLong("dividend_yield"));
 
-        LocalDate nextDividendDate = getDate(rs, "next_dividend_date");
-        LocalDate exDividendDate = getDate(rs, "ex_dividend_date");
-        LocalDate nextEarningsDate = getDate(rs, "next_earnings_date");
+        LocalDate nextDividendDate = RowMapperUtils.getDate(rs, "next_dividend_date");
+        LocalDate exDividendDate = RowMapperUtils.getDate(rs, "ex_dividend_date");
+        LocalDate nextEarningsDate = RowMapperUtils.getDate(rs, "next_earnings_date");
 
         BigDecimal peRatio = BigDecimal.valueOf(rs.getLong("pe_ratio"));
         BigDecimal beta = BigDecimal.valueOf(rs.getLong("beta"));
@@ -36,13 +35,5 @@ public class StatsRowMapper implements RowMapper<Stats> {
         return new Stats(symbol, marketcap, sharesOutstanding, employees, ttmEps, ttmDividendRate,
                 dividendYield, nextDividendDate, exDividendDate, nextEarningsDate, peRatio, beta,
                 createdAt, updatedAt);
-    }
-
-    private LocalDate getDate(ResultSet resultSet, String key) throws SQLException {
-        Date date = resultSet.getDate(key);
-        if (date == null) {
-            return null;
-        }
-        return date.toLocalDate();
     }
 }
